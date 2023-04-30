3393598a349f0b95d6977f0e9a130149c44f5606
BlackJhossy <jhossymarbalderrama@gmail.com>
2023-04-30 20:20:45 -0300

    Arreglo de Error Cors

--- src/main/java/com/backend/portafolio/controller/controller.java
+++ src/main/java/com/backend/portafolio/controller/controller.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://my-portafolio-7abab.web.app")
 @RequestMapping("/api")
 public class controller {
     
